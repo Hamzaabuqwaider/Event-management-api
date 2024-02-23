1d25a50083c0a6f3e38d564ee4d4816c713ad3ec
h.abuqwaider <hamza.abuqwaider@oaktreetec.com>
2024-02-23 15:50:37 +0300

    token

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -14,6 +14,12 @@ class EventController extends Controller
     /**
      * Display a listing of the resource.
      */
+
+    public function __construct()
+    {
+//        $this->middleware('auth:sanctum')->except(['index','show']);
+    }
+
     public function index()
     {
         $relations = ['user', 'attendees', 'attendees.user'];
@@ -38,7 +44,7 @@ class EventController extends Controller
                 'start_time' => 'required|date',
                 'end_time' => 'required|date|after:start_time'
             ]),
-            'user_id' => 1
+            'user_id' => $request->user()->id
         ]);
 
 //        return response()->json([
